9c99c8a62371f0bc77bfe42c7932a1ca92688a38
Subradeep10 <subhradeepsamanta2002@gmail.com>
2022-08-08 22:02:04 +0530

    Files init

--- backend/index.js
+++ backend/index.js
@@ -1,3 +1,21 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const app = express();
-require(dotenv).config();
+const dotenv = require('dotenv');
+dotenv.config({
+    path: './config.env'
+});
+
+// Connect to MongoDB
+mongoose.connect(process.env.MONGO_DB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+}).then(() => {
+    console.log('MongoDB Connected');
+}).catch(err => {
+    console.log(err);
+});
+
+app.listen(8000, () => {
+    console.log('Server is running on port 8000');
+})
