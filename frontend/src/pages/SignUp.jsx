b1fdd6ab623bc51e8ee9aff2b9d33640742d8f91
Subradeep10 <subhradeepsamanta2002@gmail.com>
2022-08-10 23:39:43 +0530

    firebase init

--- frontend/src/pages/SignUp.jsx
+++ frontend/src/pages/SignUp.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import MetaData from "../utils/MetaData";
 
 const SignUp = () => {
@@ -11,6 +12,43 @@ const SignUp = () => {
           src="https://assets.nflxext.com/ffe/siteui/vlv3/a1543997-c1fd-4946-92d3-b0a3648b92c7/327506e9-f0a6-4ae6-8115-97ea4c94268d/IN-en-20220808-popsignuptwoweeks-perspective_alpha_website_medium.jpg"
           alt="/"
         />
+        <div className="fixed w-full px-4 py-24 z-50">
+          <div className="max-w-[450px] h-[500px] mx-auto bg-black/75 text-white">
+            <div className="max-w-[320px] mx-auto py-16">
+              <h1 className="text-3xl font-bold">Sign Up</h1>
+              <form className="w-full flex flex-col py-4">
+                <input
+                  className="p-3 my-2 bg-gray-700 rouded"
+                  type="email"
+                  placeholder="Email"
+                  autoComplete="email"
+                />
+                <input
+                  className="p-3 my-2 bg-gray-700 rouded"
+                  type="password"
+                  placeholder="Password"
+                  autoComplete="current-password"
+                />
+                <button className="bg-red-600 py-3 my-6 rounded font-bold">
+                  Sign Up
+                </button>
+                <div className="flex justify-between items-center text-sm text-gray-600">
+                  <p>
+                    <input className="mr-2" type="checkbox" />
+                    Remember me
+                  </p>
+                  <p>Need Help?</p>
+                </div>
+                <p className="py-8">
+                  <span className="text-gray-600">
+                    Already subscribed to Netflix?
+                  </span>{" "}
+                  <Link to="/login">Sign In</Link>
+                </p>
+              </form>
+            </div>
+          </div>
+        </div>
       </div>
     </>
   );
