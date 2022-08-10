47a6048ab74a6044d43e74e8ad14bb199a0f4768
Subradeep10 <subhradeepsamanta2002@gmail.com>
2022-08-10 18:39:53 +0530

    api init

---
+++ frontend/src/config.js
@@ -0,0 +1,12 @@
+const key = "36d1c6d82888bb8b461abbb7112a27b3";
+
+const request = {
+    requestPopular: `https://api.themoviedb.org/3/movie/popular?api_key=${key}&language=en-US&page=1`,
+    requestTopRated: `https://api.themoviedb.org/3/movie/top_rated?api_key=${key}&language=en-US&page=1`,
+    requestTrending: `https://api.themoviedb.org/3/movie/popular/day?api_key=${key}&language=en-US&page=1`,
+    requestHorror: `https://api.themoviedb.org/3/discover/movie?api_key=${key}&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&with_genres=27`,
+    requestComedy: `https://api.themoviedb.org/3/discover/movie?api_key=${key}&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&with_genres=35`,
+
+}
+
+export default request;
