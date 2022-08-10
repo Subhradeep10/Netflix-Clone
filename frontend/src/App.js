95eca356ce46ee3042e86d0e9535a6b56de2aaa6
Subradeep10 <subhradeepsamanta2002@gmail.com>
2022-08-10 10:38:31 +0530

    netflix init

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,7 +2,7 @@
 function App() {
   return (
     <div className="App">
-      <h1 className='test-3xl font-bold underline'>
+      <h1 className='test-3xl font-bold text-white'>
         Hello World
       </h1>
     </div>
