053e79f63b66b952e7499c4aa7eb937d64540aeb
Subradeep10 <subhradeepsamanta2002@gmail.com>
2022-08-10 20:25:27 +0530

    api init

--- frontend/src/components/Main.jsx
+++ frontend/src/components/Main.jsx
@@ -21,9 +21,22 @@ const Main = () => {
           src={`https://image.tmdb.org/t/p/original/${movie?.backdrop_path}`}
           alt={movie?.title}
         />
-        <div>
-          <button>Play</button>
-          <button>Watch Later</button>
+        <div className="absolute w-full top-[20%] p-4 md:p-8">
+          <h1 className="text-3xl md:text-5xl font-bold">{movie?.title}</h1>
+          <div className="my-4">
+            <button className="border bg-gray-300 text-black border-gray-300 py-2 px-5">
+              Play
+            </button>
+            <button className="border text-white border-gray-300 py-2 px-5 ml-4">
+              Watch Later
+            </button>
+          </div>
+          <p className="text-gray-400 text-sm">
+            Released: {movie?.release_date}
+          </p>
+          <p className="w-full md:max-w-[70%] lg:max-w-[50%] xl:max-w-[35%] text-gray-200">
+            {movie?.overview}
+          </p>
         </div>
       </div>
     </div>
