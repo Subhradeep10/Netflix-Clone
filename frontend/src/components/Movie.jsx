6527995c88949461cf0c95ee5e0414eace658c7d
Subradeep10 <subhradeepsamanta2002@gmail.com>
2022-08-11 08:55:12 +0530

    auth done

--- frontend/src/components/Movie.jsx
+++ frontend/src/components/Movie.jsx
@@ -1,25 +1,48 @@
 import React from "react";
 import { FaHeart, FaRegHeart } from "react-icons/fa";
 import { useEffect, useState } from "react";
+import { UserAuth } from "../context/AuthContext";
+import { db } from "../firebase";
+import { arrayUnion, doc, updateDoc } from "firebase/firestore";
 
 const Movie = ({ items }) => {
   const [like, setLike] = useState(false);
+  const [saved, setSaved] = useState(false);
+  const { user } = UserAuth();
+
+  const movieID = doc(db, "users", `${user?.email}`);
+  const saveShow = async () => {
+    if (user?.email) {
+      setLike(!like);
+      setSaved(true);
+      await updateDoc(movieID, {
+        savedShows: arrayUnion({
+          id: items.id,
+          title: items.title,
+          img: items.backdrop_path,
+        }),
+      });
+    } else {
+      alert("Please log in to save a movie");
+    }
+  };
   return (
     <div className="w-[160px] sm:w-[200px] md:w-[240px] lg:w-[280px] inline-block cursor-pointer relative p-2">
       <img
-        src={`https://image.tmdb.org/t/p/original/${items?.backdrop_path}`}
+        className="w-full h-auto block"
+        src={`https://image.tmdb.org/t/p/w500/${items?.backdrop_path}`}
         alt={items?.title}
       />
       <div className="absolute top-0 left-0 w-full h-full hover:bg-black/80 opacity-0 hover:opacity-100 text-white">
-        <p>
-          {like ? (
-            <FaHeart className="absolute top-100 left-10 text-gray-100" />
-          ) : (
-            <FaRegHeart />
-          )}
-        </p>
         <p className="white-space-normal text-xs md:text-sm font-bold flex justify-center items-center h-full text-center">
           {items?.title}
+        </p>
+        <p onClick={saveShow}>
+          {like ? (
+            <FaHeart className="absolute top-4 left-4 text-gray-300" />
+          ) : (
+            <FaRegHeart className="absolute top-4 left-4 text-gray-300" />
+          )}
         </p>
       </div>
     </div>
