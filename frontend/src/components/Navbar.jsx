9e7e76d8572e5892201de403f7d9a08cdbd755e1
Subradeep10 <subhradeepsamanta2002@gmail.com>
2022-08-11 08:20:58 +0530

    auth done

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -35,7 +35,7 @@ const Navbar = () => {
 
             <button
               onClick={handleLogOut}
-              className="text-white cursor-pointer px-4 py-2 rounded-md"
+              className="bg-red-600 px-4 py-2 rounded-md text-white cursor-pointer"
             >
               Logout
             </button>
--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,8 +1,24 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
 import MetaData from "../utils/MetaData";
+import { UserAuth } from "../context/AuthContext";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const { user, logIn } = UserAuth();
+  const navigate = useNavigate();
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      await logIn(email, password);
+      navigate("/");
+    } catch (error) {
+      toast.error("Wrong email or password");
+    }
+  };
   return (
     <>
       <MetaData title="Netflix" />
@@ -16,14 +32,19 @@ const Login = () => {
           <div className="max-w-[450px] h-[500px] mx-auto bg-black/75 text-white">
             <div className="max-w-[320px] mx-auto py-16">
               <h1 className="text-3xl font-bold">Sign In</h1>
-              <form className="w-full flex flex-col py-4">
+              <form
+                onSubmit={handleSubmit}
+                className="w-full flex flex-col py-4"
+              >
                 <input
+                  onChange={(e) => setEmail(e.target.value)}
                   className="p-3 my-2 bg-gray-700 rouded"
                   type="email"
                   placeholder="Email"
                   autoComplete="email"
                 />
                 <input
+                  onChange={(e) => setPassword(e.target.value)}
                   className="p-3 my-2 bg-gray-700 rouded"
                   type="password"
                   placeholder="Password"
@@ -32,6 +53,7 @@ const Login = () => {
                 <button className="bg-red-600 py-3 my-6 rounded font-bold">
                   Sign In
                 </button>
+                <ToastContainer />
                 <div className="flex justify-between items-center text-sm text-gray-600">
                   <p>
                     <input className="mr-2" type="checkbox" />
